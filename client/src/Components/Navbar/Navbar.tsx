1251dca72ff03e1aa8ecb2e860e545162180d122
Victor Pestana <60305666+stringVictorinox@users.noreply.github.com>
2023-02-02 02:59:53 -0400

    creación-carpetas-navbar

---
+++ client/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <header>
+        <h1>¡Hola!</h1>
+    </header>
+  )
+}
--- client/src/Containers/Home/HomeContainer.tsx
+++ client/src/Containers/Home/HomeContainer.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
 import ContenedorHome from '../../Components/Home/style';
 import Home from '../../Components/Home/Home';
+import Navbar from '../../Components/Navbar/Navbar';
+
 type Props = {}
 
 const HomeContainer = (props: Props) => {
   return (
     <ContenedorHome>
+
+      <Navbar />
+      
       <Home
         name='Mario'
         apellido='Parra'
