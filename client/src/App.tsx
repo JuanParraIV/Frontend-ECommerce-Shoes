da51f31507147dee3429fa935adaaaab276ae082
Victor Pestana <60305666+stringVictorinox@users.noreply.github.com>
2023-02-04 16:59:56 -0400

    Creación del footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import HomePage from './Pages/Home/HomePage';
 import FormCreateProduct from './Components/Form/Form';
 
-function App() {
+function App(){
 
   return (
     <BrowserRouter>
--- client/src/Containers/Home/HomeContainer.tsx
+++ client/src/Containers/Home/HomeContainer.tsx
@@ -1,6 +1,7 @@
 import ContenedorHome from '../../Components/Home/style';
 import Navbar from '../../Components/Navbar/Navbar';
 import Banner from '../../Components/Banner/Banner';
+import Footer from '../../Components/Footer/Footer';
 import { useDispatch, useSelector } from 'react-redux'
 import dates from '../../../sneaker.json'
 import Product from '../../Components/Product/Product';
@@ -23,21 +24,10 @@ const HomeContainer = (props: Props) => {
 
     {/* PRUEBA PARA MOSTRAR LOS PRODUCTOS */}
     <MayLikeProducts props={dates.sneakers} />
-    {
-        dates.sneakers?.map(info => {
-          console.log(info)
-          return (
-            <Product
-            brand_name={info.brand_name}
-            color={info.color}
-            gender={info.gender}
-            grid_picture_url={info.grid_picture_url}
-            retail_price_cents={info.retail_price_cents}
-            />
-          )
-        })
-    }
+   
      </main>
+
+    <Footer />
     </ContenedorHome>
   );
 };
