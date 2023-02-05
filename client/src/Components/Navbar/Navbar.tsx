9285e5db059e756ecc3c80467b094145286c1079
Victor Pestana <60305666+stringVictorinox@users.noreply.github.com>
2023-02-04 21:28:21 -0400

    sección marcas - navbar

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,10 @@
 import icons from '../../assets/icons-navbar/icons-navbar'
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <header className="flex bg-gray-200">
-         <div className="flex w-full justify-between  h-14 mt-2 mb-2 mx-5">
+    <header className="flex bg-gray-200 flex-col">
+         <div className="flex justify-between  h-12 mt-2 mb-2 mx-5">
           
          <div className="bg-gray-3">
           <img src={icons.Logo} className="w-14 h-14" alt="" />
@@ -16,6 +17,39 @@ export default function Navbar() {
             <img src={icons.Cart} alt="Shopping_Cart" className='h-12 w-12 cursor-pointer' />
 		      </div>
 
+         </div>
+
+         <div>
+         <div className="flex justify-center bg-gray-500 py-2 mt-2">
+        	<ul className="flex">
+          		<li className="mr-6">
+            <Link to={"/vino"}>
+              <option className="text-yellow-400 text-lg font-bold hover:text-yellow-500 cursor-pointer">
+                AIR JORDAN
+              </option>
+            </Link>
+          </li>
+
+          <li className="mr-6">
+            <Link to={"/whisky"}>
+              <option className="text-yellow-400 text-lg font-bold hover:text-yellow-500 cursor-pointer">
+                ADIDAS
+              </option>
+            </Link>
+          </li>
+
+          <li className="mr-6">
+            <Link to={"/cristaleria"}>
+              <option
+                value="cristaleria"
+                className="text-yellow-400 text-lg font-bold hover:text-yellow-500 cursor-pointer"
+              >
+                NIKE
+              </option>
+            </Link>
+          </li>
+        </ul>
+      </div>
          </div>
     </header>
   )
