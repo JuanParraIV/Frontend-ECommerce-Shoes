ed0ae6e6d87dc5601b390c651f40142bd0330b29
JuanParraIV <jmparra1993@gmail.com>
2023-02-06 19:37:36 -0500

    fix: Margin and gap of product Card

--- client/src/Components/ProductFeed/styles.tsx
+++ client/src/Components/ProductFeed/styles.tsx
@@ -2,4 +2,4 @@
 import styled from "styled-components";
 import tw from "twin.macro";
 
-export const CardsContainer = styled.div`${tw`grid gap-x-1 gap-y-3 grid-cols-3`}`; 
+export const CardsContainer = styled.div`${tw`grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-24 mx-auto`}`;
