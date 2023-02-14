f7e7bfcf0b45d7756e9adcd9322681554c072b30
SoniaPatriciaSantacruz-2 <ledangom@gmail.com>
2023-02-13 21:02:59 -0500

    post-create-product

--- client/src/Components/FormCreateProduct/validation.ts
+++ client/src/Components/FormCreateProduct/validation.ts
@@ -1,11 +1,11 @@
 
 interface FormData {
   name: string;
-  price: number;
-  image: string;
+  retail_price_cents: number;
+  grid_picture_url: string;
   stock: number;
-  brand: string;
-  description: string;
+  brand_name: string;
+  details: string;
   status: string;
   category_name: string;
   color: string
@@ -14,11 +14,11 @@ interface FormData {
 
 interface Errors {
   name: string;
-  price: string;
-  image: string;
+  retail_price_cents: string;
+  grid_picture_url: string;
   stock: string;
-  brand: string;
-  description: string;
+  brand_name: string;
+  details: string;
   status: string;
   category_name: string;
   color: string
@@ -31,16 +31,16 @@ const IMAGE_REGEX = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-
 const validateName = (name: string): string =>
   !name ? 'Please introduce a name' : NAME_REGEX.test(name) ? '' : 'Only letters and spaces';
 
-const validatePrice = (price: number): string =>
-  !price ? 'Please introduce a price' : price < 1 ? 'not negative numbers' : '';
+const validatePrice = (retail_price_cents: number): string =>
+  !retail_price_cents ? 'Please introduce a price' : retail_price_cents < 1 ? 'not negative numbers' : '';
 
-const validateImage = (image: string): string =>
-  !image ? 'Please introduce a image' : IMAGE_REGEX.test(image) ? '' : 'Wrong URL';
+const validateImage = (grid_picture_url: string): string =>
+  !grid_picture_url ? 'Please introduce a image' : IMAGE_REGEX.test(grid_picture_url) ? '' : 'Wrong URL';
 
 const validateStock = (stock: number): string =>
   !stock ? 'Please introduce a stock' : stock < 1 ? 'not negative numbers' : '';
 
-const validateBrand = (brand: string): string => !brand ? 'Brand is required' : '';
+const validateBrand = (brand_name: string): string => !brand_name ? 'Brand is required' : '';
 
 const validateColor = (color: string): string => !color ? 'Color is required' : '';
 
@@ -48,20 +48,20 @@ const validateSize = (size_range: string): string => !size_range ? 'Size is requ
 
 const validateCategory = (category_name: string): string => !category_name ? 'Category is required' : '';
 
-const validateDescription = (description: string): string =>
-  !description ? 'Please introduce a description' : '';
+const validateDescription = (details: string): string =>
+  !details? 'Please introduce a description' : '';
 
 const validateStatus = (status: string): string => !status ? 'Please introduce a status' : '';
 
 export default function validateProductForm(formData: FormData): Errors {
   return {
     name: validateName(formData.name),
-    price: validatePrice(formData.price),
-    image: validateImage(formData.image),
+    retail_price_cents: validatePrice(formData.retail_price_cents),
+    grid_picture_url: validateImage(formData.grid_picture_url),
     stock: validateStock(formData.stock),
-    brand: validateBrand(formData.brand),
+    brand_name: validateBrand(formData.brand_name),
     category_name: validateCategory(formData.category_name), 
-    description: validateDescription(formData.description),
+    details: validateDescription(formData.details),
     status: validateStatus(formData.status),
     color: validateColor (formData.color),
     size_range: validateSize(formData.size_range)
