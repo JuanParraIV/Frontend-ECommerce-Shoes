24772d5431aa61e4edca893ccd7e8f5afc91e7cc
JuanParraIV <jmparra1993@gmail.com>
2023-02-16 11:00:34 -0500

    fix: Profile Form & Edit Profile submit

--- client/src/Components/Profile/Profile.tsx
+++ client/src/Components/Profile/Profile.tsx
@@ -1,79 +1,78 @@
 import { useAuthStore } from '@/App/store/useAuthStore';
 import produce from 'immer';
-import React from 'react'
+import React, { useEffect } from 'react';
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
-import logo from '../../assets/icons-navbar/Logo.png'
+import logo from '../../assets/icons-navbar/Logo.png';
 import { Link } from 'react-router-dom';
 
 function Profile() {
 
-    const {profile} = useAuthStore();
-    console.log('profile', profile)
-
+  const { profile } = useAuthStore();
+  console.log('profile', profile);
 
   return (
     <>
-    <Navbar/>
-    <main className="profile-page">
-  <section className="relative py-16 bg-blueGray-200">
-        <h1 className='flex justify-center font-bold text-5xl text-yellow-200 underline decoration-orange-200'>Your Profile</h1>
-    <div className="container mx-auto px-5">
-      <div className="relative  min-w-0 break-words bg-gray-100 w-85 mb-6 shadow-xl rounded-lg -mt-58">
-        <div className="px-6">
-          <div className="text-center mt-12">
-            <h3 className="text-4xl font-semibold leading-normal mb-2 text-blueGray-700">
-              Name: {profile.firstName} {profile.lastName}
-            </h3>
-            <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
-              <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
-              UserName: {profile.userName}
-            </div>
-        
-            <div className="mb-2 text-blueGray-600 mt-10">
-              <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>Identity Document Number:{profile.dni}
-            </div>
-            <div className="mb-2 text-blueGray-600 mt-10">
-              <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>Address:{profile.buyerAddress}
-            </div>
-            <div className="mb-2 text-blueGray-600">
-              <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>Email: {profile.email}
-            </div>
-            <div className="mb-2 text-blueGray-600">
-              <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>Number: {profile.contactNumber}
+      <Navbar />
+      <main className="profile-page">
+        <section className="relative py-16 bg-blueGray-200">
+          <h1 className='flex justify-center font-bold text-5xl text-yellow-200 underline decoration-orange-200'>Your Profile</h1>
+          <div className="container mx-auto px-5">
+            <div className="relative  min-w-0 break-words bg-gray-100 w-85 mb-6 shadow-xl rounded-lg -mt-58">
+              <div className="px-6">
+                <div className="text-center mt-12">
+                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-blueGray-700">
+                    Name: {profile.firstName} {profile.lastName}
+                  </h3>
+                  <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                    <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
+                    UserName: {profile.userName}
+                  </div>
+
+                  <div className="mb-2 text-blueGray-600 mt-10">
+                    <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>Identity Document Number:{profile.dni}
+                  </div>
+                  <div className="mb-2 text-blueGray-600 mt-10">
+                    <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>Address:{profile.buyerAddress}
+                  </div>
+                  <div className="mb-2 text-blueGray-600">
+                    <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>Email: {profile.email}
+                  </div>
+                  <div className="mb-2 text-blueGray-600">
+                    <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>Number: {profile.contactNumber}
+                  </div>
+                </div>
+                <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
+                  <div className="flex flex-wrap justify-center">
+                    <Link to='/editprofile'>
+                      <button className="font-normal text-pink-500">Edit Profile</button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
-            <div className="flex flex-wrap justify-center">
-              <Link to='/editprofile'>
-                <button className="font-normal text-pink-500">Edit Profile</button>      
-              </Link>
+          <footer className="relative bg-blueGray-200 pt-8 pb-6 mt-8">
+            <div className="container mx-auto px-4">
+              <div className="flex flex-wrap items-center md:justify-between justify-center">
+                <div className="w-full md:w-6/12 px-4 mx-auto text-center">
+                  <img
+                    src={logo}
+                    alt="Jm-zon"
+                    className="cursor-pointer w-[150px] h-[40px] object-contain"
+                  />
+                  <div className="text-sm text-blueGray-500 font-semibold py-1">
+                    Sneakers Society
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <footer className="relative bg-blueGray-200 pt-8 pb-6 mt-8">
-  <div className="container mx-auto px-4">
-    <div className="flex flex-wrap items-center md:justify-between justify-center">
-      <div className="w-full md:w-6/12 px-4 mx-auto text-center">
-        <img
-              src={logo}
-              alt="Jm-zon"
-              className="cursor-pointer w-[150px] h-[40px] object-contain"
-            />
-        <div className="text-sm text-blueGray-500 font-semibold py-1">
-          Sneakers Society
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
-  </section>
-</main>
-    <Footer/>
+          </footer>
+        </section>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default Profile;
--- client/src/Components/ProfileForm/ProfileForm.tsx
+++ client/src/Components/ProfileForm/ProfileForm.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import swal from 'sweetalert'
-import api from '@/Api/backend_sneakers'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import swal from 'sweetalert';
+import api from '@/Api/backend_sneakers';
 import Logo from '../Shared/Logo';
 import SubmitButton from '../Shared/Form/submitButton';
 import { Link } from 'react-router-dom';
@@ -14,21 +14,22 @@ import { useAuthStore } from '@/App/store/useAuthStore';
 
 
 interface FormData {
+  id?: number,
   userName: string;
   firstName: string;
   lastName: string;
   contactNumber: string;
   buyerAddress: string;
-  email: string; 
-  password: string; 
-  dni: string
+  email: string;
+  password: string;
+  dni: string;
 }
 
 
-const  EditProfileUser = () => {
+const EditProfileUser = () => {
   //const router = useRouter();
   const navigate = useNavigate();
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState<FormData>();
   const [errors, setErrors] = useState({
     userName: '',
     firstName: '',
@@ -36,70 +37,73 @@ const  EditProfileUser = () => {
     contactNumber: '',
     buyerAddress: '',
     email: '',
-    password: '', 
-    dni: ''})
-  
+    password: '',
+    dni: ''
+  });
+
   const [form, setForm] = useState<FormData>({
+    id: 0,
     userName: '',
     firstName: '',
     lastName: '',
     contactNumber: '',
     buyerAddress: '',
     email: '',
-    password: '', 
+    password: '',
     dni: ''
   });
 
 
-  const {token} = useAuthStore(state => state);
+  const { token, profile, getProfile } = useAuthStore(state => state);
+
 
   const handleSubmit = async (data: FormData) => {
-console.log(data)
-     try {
-      await api.put('/user', 
-      data
-      ,{
-        headers:{
-          'Content-Type': 'application/json',
-          Authorization:`Bearer ${token}`
-        }   
-      }
-       );
-       setForm({
+    console.log(data);
+    try {
+      await api.put('/user',
+        data
+        , {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+      setForm({
         userName: '',
         firstName: '',
         lastName: '',
         contactNumber: '',
         buyerAddress: '',
         email: '',
-        password: '', 
-         dni: ''
-             });
-       try {
-         swal({
+        password: '',
+        dni: ''
+      });
+      getProfile();
+      try {
+        swal({
           title: "Excellent",
           text: "Profile edited successfuly!",
           icon: "success",
-          button: "Ok",
-        });       
-       } catch (error) {
-       console.log(error)
-       }      
-       navigate('/profile')
-      
-     } catch (error) {
-       console.log(error);
-     }
-   };
-  
+          buttons: {
+            confirm: {
+              text: "Ok",
+              value: true,
+              visible: true,
+              className: "rounded-3xl bg-yellow-400 text-black text-center w-full px-5 py-2 my-8",
+              closeModal: true,
+            },
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+      navigate('/profile');
 
-  const routePutUsers = async () => {
-    const response = await api.get('/user');
-    return response.data
-  }
-
-
-  
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleChange = (event: React.FormEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = event.target as HTMLInputElement | HTMLSelectElement;
@@ -108,23 +112,28 @@ console.log(data)
       ...form,
       [name]: value,
     }));
-   
-  }
 
+  };
 
   useEffect(() => {
-    const getAllUsers = async () => {
-      const allUsers = await  routePutUsers();
-      if (allUsers) setUser(allUsers);
-    }
-    getAllUsers();
-  }, [])
-  console.log(form)
-  console.log(user)
+    if (profile) setUser(profile);
+  }, [profile]);
 
-
- 
-
+  useEffect(() => {
+    if (user) setForm({
+      id: user.id,
+      userName: user.userName,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      contactNumber: user.contactNumber,
+      buyerAddress: user.buyerAddress,
+      email: user.email,
+      password: user.password,
+      dni: user.dni
+    });
+  }, [user]);
+  console.log(form);
+  console.log(user);
 
   return (
     <div className='bg-neutral-300 w-50'>
@@ -136,146 +145,146 @@ console.log(data)
         <h1 className='text-center text-2xl text-[#F53F00] mt-10'>Edit Your Profile</h1>
         <div className='flex relative flex-col  items-center justify-center gap-6 grid grid-cols-2 gap-4'>
           <div className='flex justify-end items-center relative'>User Name
-              <input
+            <input
               className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe'
               type='text'
               name='userName'
               placeholder='User Name'
               value={form.userName}
               onChange={handleChange} />
-            
+
             <div className="text-red-700 underline decoration-pink-500">
-                {errors.userName ?
-                              <p className='bg-red;'>{errors.userName}</p> : null 
-                          }
+              {errors.userName ?
+                <p className='bg-red;'>{errors.userName}</p> : null
+              }
 
             </div>
           </div>
           <div className='flex justify-end items-center relative'>First Name
-              <input
+            <input
               className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe'
               type='text'
               name='firstName'
               placeholder='First Name'
               value={form.firstName}
-              onChange={handleChange} 
-              />
-            
+              onChange={handleChange}
+            />
+
             <div className="text-red-700 underline decoration-pink-500">
-                {errors.firstName?
-                              <p className='bg-red;'>{errors.firstName}</p> : null 
-                          }
+              {errors.firstName ?
+                <p className='bg-red;'>{errors.firstName}</p> : null
+              }
 
             </div>
-            </div>
+          </div>
 
-            <div className='flex justify-end items-center relative'>Last Name
-              <input
+          <div className='flex justify-end items-center relative'>Last Name
+            <input
               className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe'
               type='text'
               name='lastName'
               placeholder='Last Name'
               value={form.lastName}
               onChange={handleChange} />
-            
-            <div className="text-red-700 underline decoration-pink-500">
-                {errors.lastName?
-                              <p className='bg-red;'>{errors.lastName}</p> : null 
-                          }
-            </div>
-            </div>
 
-            
-            <div className='flex justify-end items-center relative'>Contact Number
-              <input
+            <div className="text-red-700 underline decoration-pink-500">
+              {errors.lastName ?
+                <p className='bg-red;'>{errors.lastName}</p> : null
+              }
+            </div>
+          </div>
+
+
+          <div className='flex justify-end items-center relative'>Contact Number
+            <input
               className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe'
               type='text'
               name='contactNumber'
               placeholder='Contact Number'
               value={form.contactNumber}
               onChange={handleChange} />
-            
-            <div className="text-red-700 underline decoration-pink-500">
-                {errors.contactNumber?
-                              <p className='bg-red;'>{errors.contactNumber}</p> : null 
-                          }
-            </div>
-            </div>
 
-            <div className='flex justify-end items-center relative'>Address
-              <input
+            <div className="text-red-700 underline decoration-pink-500">
+              {errors.contactNumber ?
+                <p className='bg-red;'>{errors.contactNumber}</p> : null
+              }
+            </div>
+          </div>
+
+          <div className='flex justify-end items-center relative'>Address
+            <input
               className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe'
               type='text'
               name='buyerAddress'
               placeholder='Address'
               value={form.buyerAddress}
               onChange={handleChange} />
-            
-            <div className="text-red-700 underline decoration-pink-500">
-                {errors.buyerAddress?
-                              <p className='bg-red;'>{errors.buyerAddress}</p> : null 
-                          }
-            </div>
-            </div>
 
-            <div className='flex justify-end items-center relative'>Email
-              <input
+            <div className="text-red-700 underline decoration-pink-500">
+              {errors.buyerAddress ?
+                <p className='bg-red;'>{errors.buyerAddress}</p> : null
+              }
+            </div>
+          </div>
+
+          <div className='flex justify-end items-center relative'>Email
+            <input
               className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe'
               type='text'
               name='email'
               placeholder='Email'
               value={form.email}
               onChange={handleChange} />
-            
-            <div className="text-red-700 underline decoration-pink-500">
-                {errors.email?
-                              <p className='bg-red;'>{errors.email}</p> : null 
-                          }
-            </div>
-            </div>
 
-            <div className='flex justify-end items-center relative'>Password
-              <input
+            <div className="text-red-700 underline decoration-pink-500">
+              {errors.email ?
+                <p className='bg-red;'>{errors.email}</p> : null
+              }
+            </div>
+          </div>
+
+          <div className='flex justify-end items-center relative'>Password
+            <input
               className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe'
               type='password'
               name='password'
               placeholder='Password'
               value={form.password}
               onChange={handleChange} />
-            
-            <div className="text-red-700 underline decoration-pink-500">
-                {errors.password?
-                              <p className='bg-red;'>{errors.password}</p> : null 
-                          }
-            </div>
-            </div>
 
-            <div className='flex justify-end items-center relative'>Identity Document
-              <input
+            <div className="text-red-700 underline decoration-pink-500">
+              {errors.password ?
+                <p className='bg-red;'>{errors.password}</p> : null
+              }
+            </div>
+          </div>
+
+          <div className='flex justify-end items-center relative'>Identity Document
+            <input
               className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe'
               type='text'
               name='dni'
               placeholder='Identity Document'
               value={form.dni}
               onChange={handleChange} />
-            
-            <div className="text-red-700 underline decoration-pink-500">
-                {errors.dni?
-                              <p className='bg-red;'>{errors.dni}</p> : null 
-                          }
-            </div>
-            </div>
 
-    
-          <div className='p-2 w-[250px]'>    
+            <div className="text-red-700 underline decoration-pink-500">
+              {errors.dni ?
+                <p className='bg-red;'>{errors.dni}</p> : null
+              }
+            </div>
+          </div>
+
+
+          <div className='p-2 w-[250px]'>
           </div >
           <div className='justify-self-center'>
-          <SubmitButton text='Edit Profile' />
+            <SubmitButton text='Edit Profile' />
           </div>
         </div>
         <span className=" rounded-lg w-[600px] h-0.5 bg-gray-200"></span>
       </form>
     </div>
-  )
-}
-export default  EditProfileUser; 
+  );
+};
+export default EditProfileUser;
